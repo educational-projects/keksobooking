9cc55cde2207e562a38aac444ed75a9fb6b1a080
ваше имя <dimarik-tixonov@yandex.ru>
2021-06-08 20:53:35 +0300

    fix: правки jsdoca

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 /**
+ * генератор случайных чисел
  * @param {number} min минимальный диапазон
  * @param {number} max максимальный диапазон
  * @param {number} float число знаков после запятой
- * @returns генератор рандомных значений
+ * @returns {number} случайное значение
  */
 function createRandomNumber(min, max, float = 0) {
   if (min < 0 || max < 0 || max < min) {
