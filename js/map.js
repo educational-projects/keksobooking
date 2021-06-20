f52c7c30ebcf281b654dde12d1b005afc039caa8
ваше имя <dimarik-tixonov@yandex.ru>
2021-06-20 15:58:19 +0300

    fix: добработка функции блокировки форм страницы

--- js/map.js
+++ js/map.js
@@ -4,8 +4,12 @@ const mapFilters = document.querySelector('.map__filters');
 const disableForm = () => {
   adForm.classList.add('ad-form--disabled');
   mapFilters.classList.add('map__filters--disabled');
+  /*
+  if (карта загрузилась) {
+    adForm.classList.remove('ad-form--disabled');
+    mapFilters.classList.remove('map__filters--disabled')
+  }
+  */
 };
-
-disableForm();
 
 export {disableForm};
