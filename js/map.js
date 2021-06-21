670076e8197b80261e17bf27e79e34b43cd046bd
ваше имя <dimarik-tixonov@yandex.ru>
2021-06-21 18:23:52 +0300

    fix: доработка функции блокировки форм

--- js/map.js
+++ js/map.js
@@ -4,12 +4,21 @@ const mapFilters = document.querySelector('.map__filters');
 const disableForm = () => {
   adForm.classList.add('ad-form--disabled');
   mapFilters.classList.add('map__filters--disabled');
-  /*
-  if (карта загрузилась) {
-    adForm.classList.remove('ad-form--disabled');
-    mapFilters.classList.remove('map__filters--disabled')
+  //* блокируем все элементы формы карты
+  const mapFiltersElements = mapFilters.children;
+  for (let i = 0; i < mapFiltersElements.length; i++) {
+    mapFiltersElements[i].setAttribute('disabled', '');
   }
-  */
+  //*
+  const adFormElements = adForm.children;
+  for (let i = 0; i < adFormElements.length; i++) {
+    adFormElements[i].setAttribute('disabled', '');
+  }
 };
 
-export {disableForm};
+const enableForm = () => {
+  adForm.classList.remove('ad-form--disabled');
+  mapFilters.classList.remove('map__filters--disabled');
+};
+
+export {disableForm, enableForm};
