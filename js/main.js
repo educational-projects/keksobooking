68b3382548682c9e6e0fac8feac264a407ebf5df
ваше имя <dimarik-tixonov@yandex.ru>
2021-06-22 21:30:34 +0300

    refactor: приведение функций к одному типу

--- js/main.js
+++ js/main.js
@@ -1,14 +1,10 @@
-import {similarOffers} from './data.js';
-import {templateCard} from './cards.js';
+import {createOffer} from './cards.js';
 import { disableForm, enableForm } from './map.js';
 //* Блокировка форм
 disableForm();
 
-//* Создание массима из 10 элементов
-similarOffers();
-
-//не понимаю что тут делаю, вызываю сгенирированную карточку вроде
-templateCard;
+//* Вызов сгенирированной карточки объявления
+createOffer;
 
 //* Разблокировка форм
 enableForm();
--- js/util.js
+++ js/util.js
@@ -5,12 +5,12 @@
  * @param {number} float число знаков после запятой
  * @returns {number | Error} случайное значение
  */
-function createRandomNumber(min, max, float = 0) {
+const createRandomNumber = (min, max, float = 0) => {
   if (min < 0 || max < 0 || max < min) {
     throw new Error('данные меньше ноля');
   }
   return Number((Math.random() * (max - min) + min).toFixed(float));
-}
+};
 
 //* Выбор случайного элемента массива
 /**
@@ -18,10 +18,10 @@ function createRandomNumber(min, max, float = 0) {
  * @param {array} array
  * @returns {array | undefined} массив со случайным элементом
  */
-function getArrayRandomElement (array) {
+const getArrayRandomElement = (array) => {
   if (array && array.length) {
     return array[Math.floor(Math.random() * array.length)];
   }
-}
+};
 
 export {createRandomNumber, getArrayRandomElement};
