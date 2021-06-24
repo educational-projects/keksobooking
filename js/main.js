f87a59a0cae6f355d7d21e65537bb2e1cbe27428
ваше имя <dimarik-tixonov@yandex.ru>
2021-06-24 18:51:27 +0300

    fix: доработка функции генерации и размещения объявления

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-import {createOffer} from './cards.js';
+import {similarOffers} from './data.js';
+import {OfferCard} from './cards.js';
 import { disableForm, enableForm } from './map.js';
 //* Блокировка форм
 disableForm();
 
 //* Вызов сгенирированной карточки объявления
-createOffer;
+OfferCard(similarOffers(1));
 
 //* Разблокировка форм
 enableForm();
